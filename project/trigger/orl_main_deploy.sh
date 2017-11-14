7c883f1f666e41081cd8f2c05cf26c7226b46b12
dishmaev <idax@rambler.ru>
2017-11-14 11:20:36 +0300

    changes

---
+++ project/trigger/orl_main_deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+###header
+
+readonly VAR_PARAMETERS='$1 script name without extenstion, $2 suite, $3 build tar.gz file name'
+
+if [ "$#" != "3" ]; then echo "Call syntax: $(basename "$0") $VAR_PARAMETERS"; exit 1; fi
+if [ -f ${1}.ok ]; then rm ${1}.ok; fi
+exec 1>${1}.log
+exec 2>${1}.err
+
+###function
+
+checkRetVal(){
+  if [ "$?" != "0" ]; then exit 1; fi
+}
+
+###body
+
+echo "Current deploy suite: $2"
+
+uname -a
+
+#sudo apt -y update
+#checkRetVal
+
+tar -xvf $3
+checkRetVal
+
+#manually install packages
+for VAR_CUR_PACKAGE in $HOME/*.deb; do
+  if [ ! -r "$VAR_CUR_PACKAGE" ]; then continue; fi
+  sudo rpm -i $VAR_CUR_PACKAGE
+  checkRetVal
+done
+
+#install packages from personal repository
+#sudo apt update
+#checkRetVal
+#sudo apt -y install cppboost
+#checkRetVal
+
+##test
+
+cppboost
+checkRetVal
+
+###finish
+
+echo 1 > ${1}.ok
+exit 0
