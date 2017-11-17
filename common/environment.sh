67491e356644574e8406af0a22d7f816b1411a88
dishmaev <idax@rambler.ru>
2017-11-17 08:40:40 +0300

    changes

--- common/environment.sh
+++ common/environment.sh
@@ -27,13 +27,13 @@ if isEmpty "$ENV_GIT_USER_NAME"; then checkNotEmptyEnvironment "ENV_GIT_USER_NAM
 readonly ENV_GIT_USER_EMAIL=$(git config user.email)
 if isEmpty "$ENV_GIT_USER_EMAIL"; then checkNotEmptyEnvironment "ENV_GIT_USER_EMAIL"; fi
 #default username for connect to hosts, run scripts, etc.
-readonly ENV_SSH_USER_NAME=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/username.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; else echo $(whoami); fi')
+readonly ENV_SSH_USER_NAME=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/data/user.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; else echo $(whoami); fi')
 if isEmpty "$ENV_SSH_USER_NAME"; then checkNotEmptyEnvironment "ENV_SSH_USER_NAME"; fi
 #file with default password for $ENV_SSH_USER_NAME
-readonly ENV_SSH_USER_PASS=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/sshpwd.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; fi')
+readonly ENV_SSH_USER_PASS=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/data/ssh_pwd.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; fi')
 if isEmpty "$ENV_SSH_USER_PASS"; then checkNotEmptyEnvironment "ENV_SSH_USER_PASS"; fi
 #ssh public keyID
-readonly ENV_SSH_KEYID=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/id_idax_rsa.pub; if [ -r $VAR_FILE_NAME ]; then echo $VAR_FILE_NAME; fi')
+readonly ENV_SSH_KEYID=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/data/ssh_keyid.pub; if [ -r $VAR_FILE_NAME ]; then echo $VAR_FILE_NAME; fi')
 if isEmpty "$ENV_SSH_KEYID"; then checkNotEmptyEnvironment "ENV_SSH_KEYID"; fi
 #distrib repository
 readonly ENV_DISTRIB_REPO='git@github.com:dishmaev/dishmaev.github.io.git'
@@ -42,7 +42,7 @@ if isEmpty "$ENV_DISTRIB_REPO"; then checkNotEmptyEnvironment "ENV_DISTRIB_REPO"
 readonly ENV_TOOLS_REPO=$(git config remote.origin.url)
 if isEmpty "$ENV_TOOLS_REPO"; then checkNotEmptyEnvironment "ENV_TOOLS_REPO"; fi
 #default password, used by ovftool, password with escaped special characters using %, for instance %40 = @, %5c = \
-readonly ENV_OVFTOOL_USER_PASS=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/ovftoolpwd.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; fi')
+readonly ENV_OVFTOOL_USER_PASS=$(eval 'VAR_FILE_NAME='$ENV_ROOT_DIR'/common/data/ovftool_pwd.txt; if [ -r $VAR_FILE_NAME ]; then cat $VAR_FILE_NAME; fi')
 if isEmpty "$ENV_OVFTOOL_USER_PASS"; then checkNotEmptyEnvironment "ENV_OVFTOOL_USER_PASS"; fi
 #local directory to save downloads
 readonly ENV_DOWNLOAD_PATH=$(if [ ! -d "$HOME/Downloads" ]; then mkdir "$HOME/Downloads"; fi; echo "$HOME/Downloads")
--- common/initialize.sh
+++ common/initialize.sh
@@ -1,41 +1,88 @@
 #!/bin/sh
 
-if [ ! -x "$(command -v git)" ]; then
-  echo 'Error: Must install git previously'
-fi
+###header
 
 echo 'Initialize general environment variables of the tools'
 
-PRM_SSH_USER_NAME=$(eval 'if [ -r $(dirname "$0")/username.txt ]; then cat $(dirname "$0")/username.txt; else echo $(whoami); fi')
-PRM_SSH_USER_PASS=$(eval 'if [ -r $(dirname "$0")/sshpwd.txt ]; then cat $(dirname "$0")/sshpwd.txt; fi')
-PRM_OVFTOOL_USER_PASS=$(eval 'if [ -r $(dirname "$0")/ovftoolpwd.txt ]; then cat $(dirname "$0")/ovftoolpwd.txt; fi')
+###function
+
+#$1 command
+checkCommand(){
+  if [ ! -x "$(command -v $1)" ]; then
+    echo "Error: Must install $1 previously"
+    exit 1
+  fi
+}
+
+##private consts
+CONST_SSH_FILE_NAME=$HOME/.ssh/id_rsa
+
+##private vars
+PRM_SSH_KEYID=$(eval 'if [ -r $(dirname "$0")/data/ssh_keyid.pub ]; then echo "$(ssh-keygen -lf $(dirname "$0")/data/ssh_keyid.pub))"; fi')
+PRM_SSH_USER_NAME=$(eval 'if [ -r $(dirname "$0")/data/user.txt ]; then cat $(dirname "$0")/data/user.txt; else echo $(whoami); fi')
+PRM_SSH_USER_PASS=$(eval 'if [ -r $(dirname "$0")/data/ssh_pwd.txt ]; then cat $(dirname "$0")/data/ssh_pwd.txt; fi')
+PRM_OVFTOOL_USER_PASS=$(eval 'if [ -r $(dirname "$0")/data/ovftool_pwd.txt ]; then cat $(dirname "$0")/data/ovftool_pwd.txt; fi')
 PRM_GIT_USER_NAME=$(git config user.name)
 PRM_GIT_USER_EMAIL=$(git config user.email)
 VAR_INPUT=''
+VAR_FINGERPRINT=''
+VAR_COUNT=''
+
+###check commands
+
+checkCommand "git"
+checkCommand "ssh-keygen"
+checkCommand "ssh-add"
+
+###body
+
+if [ "$PRM_SSH_KEYID" = "" ]; then
+  if [ ! -r $CONST_SSH_FILE_NAME ]; then
+    read -r -p "Start generate SSH pair key? [Y/n] " VAR_INPUT
+    if [ $VAR_INPUT != "Y" ] && [ $VAR_INPUT != "y" ]; then echo "Error: Private SSH key file $CONST_SSH_FILE_NAME not found"; exit 1; fi
+    ssh-keygen
+    if [ "$?" != "0" ]; then echo "Error: Must generate or install private SSH key"; exit 1; fi
+  fi
+  read -r -p "Private SSH key file? [$CONST_SSH_FILE_NAME] " VAR_INPUT
+  VAR_INPUT=${VAR_INPUT:-$CONST_SSH_FILE_NAME}
+  if [ ! -r $VAR_INPUT ]; then echo "Error: Private SSH key file $VAR_INPUT not found"; exit 1; fi
+  echo "Save changes to $(dirname "$0")/data/ssh_keyid.pub"
+  ssh-keygen -y -f $VAR_INPUT > $(dirname "$0")/data/ssh_keyid.pub
+  chmod u=r,g=,o= $(dirname "$0")/data/ssh_keyid.pub
+  eval "$(ssh-agent -s)"
+  ssh-add $VAR_INPUT
+  if [ "$?" != "0" ]; then echo "Error: Must add private SSH key to the ssh-agent, try exec 'ssh-add $VAR_INPUT' manually"; exit 1; fi
+  PRM_SSH_KEYID=$(dirname "$0")/data/ssh_keyid.pub
+fi
+
+VAR_FINGERPRINT=$(ssh-keygen -lf $PRM_SSH_KEYID)
+VAR_COUNT=$(ssh-add -l | grep "$VAR_FINGERPRINT" | wc -l)
+
+if [ "$VAR_COUNT" = "0" ]; then echo "Error: Private SSH key '$VAR_FINGERPRINT' not loaded to the ssh-agent, repeat exec 'eval "$(ssh-agent -s)"', and 'ssh-add $VAR_INPUT' manually"; exit 1; fi
 
 read -r -p "User name? [$PRM_SSH_USER_NAME] " VAR_INPUT
 VAR_INPUT=${VAR_INPUT:-$PRM_SSH_USER_NAME}
 if [ "$VAR_INPUT" != "$PRM_SSH_USER_NAME" ]; then
-  echo "Save changes to $(dirname "$0")/username.txt"
-  echo "$VAR_INPUT" > $(dirname "$0")/username.txt
-  chmod u=rw,g=,o= $(dirname "$0")/username.txt
+  echo "Save changes to $(dirname "$0")/data/user.txt"
+  echo "$VAR_INPUT" > $(dirname "$0")/data/user.txt
+  chmod u=rw,g=,o= $(dirname "$0")/data/user.txt
   PRM_SSH_USER_NAME=$VAR_INPUT
 fi
 
 read -r -p "User name '$PRM_SSH_USER_NAME' password? [$PRM_SSH_USER_PASS] " VAR_INPUT
 VAR_INPUT=${VAR_INPUT:-$PRM_SSH_USER_PASS}
 if [ "$VAR_INPUT" != "$PRM_SSH_USER_PASS" ]; then
-  echo "Save changes to $(dirname "$0")/sshpwd.txt"
-  echo "$VAR_INPUT" > $(dirname "$0")/sshpwd.txt
-  chmod u=rw,g=,o= $(dirname "$0")/sshpwd.txt
+  echo "Save changes to $(dirname "$0")/data/ssh_pwd.txt"
+  echo "$VAR_INPUT" > $(dirname "$0")/data/ssh_pwd.txt
+  chmod u=rw,g=,o= $(dirname "$0")/data/ssh_pwd.txt
 fi
 
 read -r -p "User name '$PRM_SSH_USER_NAME' OVFTool password? [$PRM_OVFTOOL_USER_PASS] " VAR_INPUT
 VAR_INPUT=${VAR_INPUT:-$PRM_OVFTOOL_USER_PASS}
 if [ "$VAR_INPUT" != "$PRM_OVFTOOL_USER_PASS" ]; then
-  echo "Save changes to $(dirname "$0")/ovftoolpwd.txt"
-  echo "$VAR_INPUT" > $(dirname "$0")/ovftoolpwd.txt
-  chmod u=rw,g=,o= $(dirname "$0")/ovftoolpwd.txt
+  echo "Save changes to $(dirname "$0")/data/ovftool_pwd.txt"
+  echo "$VAR_INPUT" > $(dirname "$0")/data/ovftool_pwd.txt
+  chmod u=rw,g=,o= $(dirname "$0")/data/ovftool_pwd.txt
 fi
 
 read -r -p "Git user name? [$PRM_GIT_USER_NAME] " VAR_INPUT
@@ -51,8 +98,5 @@ if [ "$VAR_INPUT" != "$PRM_GIT_USER_EMAIL" ]; then
   echo "Exec 'git config user.email $VAR_INPUT'"
   git config user.email $VAR_INPUT
 fi
-
-echo 'Finally check available you secret ssh key on current system. It must be shown below:'
-ssh-add -l
 
 echo 'Enjoy!'
