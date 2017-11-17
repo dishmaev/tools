3b1b872095fcfee63406a591fcff9c3a7a9923c9
dishmaev <idax@rambler.ru>
2017-11-17 11:55:42 +0300

    changes

--- common/const.sh
+++ common/const.sh
@@ -27,7 +27,7 @@ readonly COMMON_CONST_DEFAULT_VERSION='default' # default version name
 readonly COMMON_CONST_DEFAULT_VM_ROLE='main' # default version name
 readonly COMMON_CONST_DEFAULT_VM_NAME='autogen' # default vm name
 readonly COMMON_CONST_DATA_CFG_SEPARATOR='::' #default separator for data config files
-readonly COMMON_CONST_SHOW_DEBUG="$COMMON_CONST_FALSE" #if true, show log details with error trace
+readonly COMMON_CONST_SHOW_DEBUG="$COMMON_CONST_TRUE" #if true, show log details with error trace
 
 #incorp infrastructure
 readonly COMMON_CONST_INCORP_MAILSERVER_HOST='mail'
