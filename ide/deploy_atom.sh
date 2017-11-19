f62d088337e838a2ef0361f66d6e74677015a2d3
dishmaev <idax@rambler.ru>
2017-11-19 12:37:42 +0300

    changes

--- ide/deploy_atom.sh
+++ ide/deploy_atom.sh
@@ -72,7 +72,7 @@ if isAPTLinux "$VAR_LINUX_BASED"; then
   sudo apt -y install -f
   if ! isRetValOK; then exitError; fi
 elif isRPMLinux "$VAR_LINUX_BASED"; then
-  sudo yum -y install $VAR_ORIG_FILE_PATH\
+  sudo yum -y install $VAR_ORIG_FILE_PATH
   if ! isRetValOK; then exitError; fi
 fi
 apm install atom-ide-ui
