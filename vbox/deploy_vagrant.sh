2c0dc196b01be4e06997040e26e741dab59fb481
dishmaev <idax@rambler.ru>
2017-12-09 10:47:40 +0300

    changes

--- vbox/deploy_vagrant.sh
+++ vbox/deploy_vagrant.sh
@@ -5,8 +5,9 @@
 targetDescription 'Deploy Vagrant on the local OS x86_64'
 
 ##private consts
-readonly CONST_VAGRANT_URL='https://releases.hashicorp.com/vagrant/@PRM_VERSION@/vagrant_@PRM_VERSION@_x86_64.deb' #url for download
-readonly CONST_VAGRANT_VERSION='2.0.1'
+readonly CONST_FILE_APT_URL='https://releases.hashicorp.com/vagrant/@PRM_VERSION@/vagrant_@PRM_VERSION@_x86_64.deb' #url for download
+readonly CONST_FILE_RPM_URL='https://releases.hashicorp.com/vagrant/@PRM_VERSION@/vagrant_@PRM_VERSION@_x86_64.rpm' #RPM-based Linux url for download
+readonly CONST_FILE_VERSION='2.0.1'
 
 ##private vars
 PRM_VERSION='' #vagrant version
@@ -22,11 +23,13 @@ checkAutoYes "$1" || shift
 
 ###help
 
-echoHelp $# 1 '[version=$CONST_VAGRANT_VERSION]' "$CONST_VAGRANT_VERSION" "Version format 'X.X.X'. While tested only on APT-based Linux. Vagrant url https://www.vagrantup.com/downloads.html"
+echoHelp $# 1 '[version=$CONST_FILE_VERSION]' \
+"$CONST_FILE_VERSION" \
+"Version format 'X.X.X'. While tested only on APT-based Linux. Vagrant url https://www.vagrantup.com/downloads.html"
 
 ###check commands
 
-PRM_VERSION=${1:-$CONST_VAGRANT_VERSION}
+PRM_VERSION=${1:-$CONST_FILE_VERSION}
 
 checkCommandExist 'version' "$PRM_VERSION" ''
 
@@ -59,18 +62,29 @@ fi
 #check supported OS
 if ! isLinuxOS; then exitError 'not supported OS'; fi
 VAR_LINUX_BASED=$(checkLinuxAptOrRpm) || exitChildError "$VAR_LINUX_BASED"
-if ! isAPTLinux "$VAR_LINUX_BASED"; then exitError 'not supported OS'; fi
 
-VAR_FILE_URL=$(echo "$CONST_VAGRANT_URL" | sed -e "s#@PRM_VERSION@#$PRM_VERSION#g") || exitChildError "$VAR_FILE_URL"
+if isAPTLinux "$VAR_LINUX_BASED"; then
+  VAR_FILE_URL="$CONST_FILE_APT_URL"
+elif isRPMLinux "$VAR_LINUX_BASED"; then
+  VAR_FILE_URL="$CONST_FILE_RPM_URL"
+else
+  exitError "unknown Linux based package system"
+fi
+VAR_FILE_URL=$(echo "$VAR_FILE_URL" | sed -e "s#@PRM_VERSION@#$PRM_VERSION#g") || exitChildError "$VAR_FILE_URL"
 VAR_ORIG_FILE_NAME=$(getFileNameFromUrlString "$VAR_FILE_URL") || exitChildError "$VAR_ORIG_FILE_NAME"
 VAR_ORIG_FILE_PATH=$ENV_DOWNLOAD_PATH/$VAR_ORIG_FILE_NAME
 if ! isFileExistAndRead "$VAR_ORIG_FILE_PATH"; then
   wget -O $VAR_ORIG_FILE_PATH $VAR_FILE_URL
   checkRetValOK
 fi
-checkDpkgUnlock
-sudo apt -y install $VAR_ORIG_FILE_PATH
-checkRetValOK
+if isAPTLinux "$VAR_LINUX_BASED"; then
+  checkDpkgUnlock
+  sudo apt -y install $VAR_ORIG_FILE_PATH
+  checkRetValOK
+elif isRPMLinux "$VAR_LINUX_BASED"; then
+  sudo apt -y install $VAR_ORIG_FILE_PATH
+  checkRetValOK
+fi
 
 vagrant --version
 
