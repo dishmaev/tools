86f03606ef77de41174525035f2c750d38affbe7
dishmaev <idax@rambler.ru>
2017-12-10 18:30:22 +0300

    changes

--- common/environment.sh
+++ common/environment.sh
@@ -3,7 +3,8 @@
 ##using files: consts.sh, function.sh
 
 #set correct path before using this tools
-readonly ENV_ROOT_DIR=$(getParentDirectoryPath "$(realpath $(dirname "$0"))")
+#readonly ENV_ROOT_DIR=$(getParentDirectoryPath "$(realpath $(dirname "$0"))")
+readonly ENV_ROOT_DIR=$(cd $(dirname "$0")/..; pwd)
 if isEmpty "$ENV_ROOT_DIR"; then checkNotEmptyEnvironment "ENV_ROOT_DIR"; fi
 #script file name
 readonly ENV_SCRIPT_FILE_NAME=$(basename "$0")
